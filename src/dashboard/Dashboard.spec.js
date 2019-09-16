bd63cd51374d88c6961c80683e8e8dd54d0ec84f
Derek Etman <Dereketman@gmail.com>
2019-09-16 14:25:05 -0700

    Dashboard Test Written & sucessful

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,30 @@
 // Test away
+import React from 'react';
+import {render} from '@testing-library/react';
+import {renderer} from 'react-test-renderer';
+
+import Dashboard from './Dashboard';
+
+test('Dashboard is rendering without crashing', () => {
+    render(<Dashboard />);
+});
+
+test('DashBoard Snapshot matches Dashboard render', () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+});
+
+
+// ### Gate tests ###
+// - defaults to `unlocked` and `open`
+// - cannot be closed or opened if it is locked
+
+test('Dashboard Defaults to unlocked and open', () => {
+    const {getByText} = render(<Dashboard />);
+    getByText(/unlocked/i);
+    getByText(/open/i);
+});
+
+test('Cannot be closed or opened if it is locked', () => {
+
+});
+
