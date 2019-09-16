ad04e10d0c7466b68c38d62aa76edbd070434e96
Derek Etman <Dereketman@gmail.com>
2019-09-16 16:05:36 -0700

    Display finished except Class checks

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Display from './Display';
+
 // ### Display Component####
 
 // - displays if gate is open/closed and if it is locked/unlocked
@@ -5,3 +10,32 @@
 // - displays 'Locked' if the `locked` prop is `true` and 'Unlocked' if otherwise
 // - when `locked` or `closed` use the `red-led` class
 // - when `unlocked` or `open` use the `green-led` class
+
+describe('Display Tests', () => {
+
+test('Displays gate and Locked', () => {
+    let gateStatus = render(<Display closed={true}/>);
+    let lockStatus = render(<Display locked={true} />);
+    expect(gateStatus.getByText("Closed"));
+    expect(lockStatus.getByText("Locked"));
+    
+
+})
+
+test('Displays if gate is open/closed ',() => {
+    let displayClosed = render(<Display closed={true} />);
+    let displayOpen = render(<Display closed={false} />);
+    expect(displayOpen.getByText("Open"));
+    expect(displayClosed.getByText("Closed"));
+
+});
+
+test('Displays if gate is locked/unlocked',() => {
+    let displayLockClose = render (<Display locked={true} />);
+    let displayLockOpen = render(<Display locked={false} />);
+    expect(displayLockClose.getByText("Locked"));
+    expect(displayLockOpen.getByText("Unlocked"));
+
+});
+
+});
