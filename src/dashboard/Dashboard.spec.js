3e44c3cb98b065d1a3c40805a7ecb96e3bf06d6c
Derek Etman <Dereketman@gmail.com>
2019-09-16 17:03:35 -0700

    Dashboard all works

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -4,27 +4,34 @@ import {render} from '@testing-library/react';
 import {renderer} from 'react-test-renderer';
 
 import Dashboard from './Dashboard';
+import Controls from '../controls/Controls'
 
-test('Dashboard is rendering without crashing', () => {
-    render(<Dashboard />);
-});
+describe('Dashboard works', () => {
 
-test('DashBoard Snapshot matches Dashboard render', () => {
-    expect(render(<Dashboard />)).toMatchSnapshot();
-});
+    test('Dashboard is rendering without crashing', () => {
+        render(<Dashboard />);
+    });
+
+    test('DashBoard Snapshot matches Dashboard render', () => {
+        expect(render(<Dashboard />)).toMatchSnapshot();
+    });
 
 
-// ### Gate tests ###
-// - defaults to `unlocked` and `open`
-// - cannot be closed or opened if it is locked
+    // ### Gate tests ###
+    // - defaults to `unlocked` and ``open`
+    // - cannot be closed or opened if it is locked
 
-test('Dashboard Defaults to unlocked and open', () => {
-    const {getByText} = render(<Dashboard />);
-    getByText(/unlocked/i);
-    getByText(/open/i);
+    test('Dashboard Defaults to unlocked and open', () => {
+        const {getByText} = render(<Dashboard />);
+        getByText(/unlocked/i);
+        getByText(/open/i);
+    });
+
+    
 });
 
 test('Cannot be closed or opened if it is locked', () => {
+    const {getByText} = render(<Controls closed={true} locked={true}  />);
+    expect(getByText('Open Gate').disabled).toBe(true);
 
 });
-
